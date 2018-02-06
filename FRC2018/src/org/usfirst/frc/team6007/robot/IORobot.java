7cbe335b2c6be9edfb6c32286acb4d5b166607d0
Ebony Nicholas <35989154+EbonyN@users.noreply.github.com>
2018-02-06 14:21:52 +1100

    Update IORobot.java

--- FRC2018/src/org/usfirst/frc/team6007/robot/IORobot.java
+++ FRC2018/src/org/usfirst/frc/team6007/robot/IORobot.java
@@ -1,5 +1,7 @@
 package org.usfirst.frc.team6007.robot;
 
+import com.kauailabs.navx.frc.AHRS;
+
 import edu.wpi.first.wpilibj.Spark;
 import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.Joystick;
@@ -9,5 +11,18 @@ import edu.wpi.first.wpilibj.RobotDrive.MotorType;
 
 
 public class IORobot(){
+  
+  AHRS ahrs;
+ // Joystick driverStick;
+  
+  
+}
 
+public IORobot(){
+  try{
+  ahrs = new AHRS(SPI.Port.kMXP); 
+  }
+  catch (RuntimeException ex ){
+     DriverStation.reportError("Error instantiating navX MXP:  " + ex.getMessage(), true);
+  }
 }
