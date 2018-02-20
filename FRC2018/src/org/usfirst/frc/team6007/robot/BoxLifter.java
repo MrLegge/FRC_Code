48c3f7bce0ba29fb00d86be4d753d259083978d5
Mr Phil Legge <phil.legge@det.nsw.edu.au>
2018-02-20 15:19:51 +1100

    Update BoxLifter.java

--- FRC2018/src/org/usfirst/frc/team6007/robot/BoxLifter.java
+++ FRC2018/src/org/usfirst/frc/team6007/robot/BoxLifter.java
@@ -1,50 +1,66 @@
-
-//imported relevent libraries
+/* this is the code that brings the external libries in for our use  */
 package org.usfirst.frc.team6007.robot;
 
 import edu.wpi.first.wpilibj.Spark;
+import edu.wpi.first.wpilibj.SpeedControllerGroup;
 import edu.wpi.first.wpilibj.DigitalInput;
-import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.Timer;
-import edu.wpi.first.wpilibj.RobotDrive;
-import edu.wpi.first.wpilibj.RobotDrive.MotorType; 
+import edu.wpi.first.wpilibj.drive.DifferentialDrive; 
+import edu.wpi.first.wpilibj.drive.RobotDriveBase;
+import edu.wpi.first.wpilibj.VictorSP;
+import edu.wpi.first.wpilibj.command.PIDSubsystem;
 
-
-
-
-public class BoxLifter(){
- 
- RobotDrive lifterBase;
- Joystick driverStick;
- 
- private Spark leftLifterMotor;
- private Spark rightLifterMotor;
- final int LEFT_LIFTER_MOTOR_ID = 2;  //Change these depending on pin configuration
- final int RIGHT_LIFTER_MOTOR_ID = 3; //Change these depending on pin configuration
- private double MotorSpeed = 0.5;
-  
-  //constructor
-  
-  public BoxLifter(){
-   
-  leftLifterMotor = new Spark(LEFT_LIFTER_MOTOR_ID);
-  rightLifterMotor = new Spark(RIGHT_LIFTER_MOTOR_ID);
-	  
-  driverStick = new Joystick(0);
+/* this is the declaraion of the class everything must be within these curly braces  */
+public class BoxLifter extends PIDSubsystem{
 	
-  grabberBase = new RobotDrive(leftLifterMotor, rightLifterMotor);
-   
-  }
-r
-
-   public void liftUp(){
-		
-   }
+	/* sets asside some space to make these objects will be declared later  */
 	
-   public void liftDown(){
+	DifferentialDrive lifterBase;
+
+	
+	/* select correct controller for robot configuration  */
+	private VictorSP topLifterMotor;
+	private VictorSP bottomLifterMotor;
+	// private Spark topLifterMotor;
+	// private Spark bottomLifterMotor;
+
+	
+	
+
+	/* this is the constructor that 'builds' the object when called  */
+	public BoxLifter(){
+
+		topLifterMotor = new VictorSP(RobotMap.RIGHT_TOP_LIFTER_MOTOR_ID);
+		bottomLifterMotor = new VictorSP(RobotMap.RIGHT_BOTTOM_LIFTER_MOTOR_ID);
+		//topLifterMotor = new Spark(RIGHT_TOP_LIFTER_MOTOR_ID);
+		//bottomLifterMotor = new Spark(RIGHT_BOTTOM_LIFTER_MOTOR_ID);
 		
-   }
+		bottomLifterMotor.setInverted(true);
 
+		lifterBase = new DifferentialDrive(topLifterMotor, bottomLifterMotor);
 
+		lifterBase.setExpiration(0.1);
+	
 }
+	
+	/* function to pull the power cube into the holder  */
+	public void liftUp(double liftPower){
+
+		lifterBase.arcadeDrive(liftPower, 0);
+
+	}
+	
+	/* function to eject the power cube from the holder  */
+	public void placeDown(double lowerPower){
+
+		lifterBase.arcadeDrive(lowerPower, 0);	
+	
+	}
+		
+}
+
+
+
+
+
 
