d19335fe5693ef169310e4bcd9956d60e548e167
Mr Phil Legge <phil.legge@det.nsw.edu.au>
2018-03-01 15:25:01 +1100

    Update BoxLifter.java

--- FRC2018/src/org/usfirst/frc/team6007/robot/BoxLifter.java
+++ FRC2018/src/org/usfirst/frc/team6007/robot/BoxLifter.java
@@ -1,13 +1,9 @@
 /* this is the code that brings the external libries in for our use  */
 package org.usfirst.frc.team6007.robot;
 
-import edu.wpi.first.wpilibj.Spark;
-import edu.wpi.first.wpilibj.SpeedControllerGroup;
-import edu.wpi.first.wpilibj.DigitalInput;
-import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive; 
-import edu.wpi.first.wpilibj.drive.RobotDriveBase;
-import edu.wpi.first.wpilibj.VictorSP;
+//import edu.wpi.first.wpilibj.VictorSP;
+import edu.wpi.first.wpilibj.Spark;
 import edu.wpi.first.wpilibj.command.PIDSubsystem;
 
 /* this is the declaraion of the class everything must be within these curly braces  */
@@ -15,30 +11,29 @@ public class BoxLifter extends PIDSubsystem{
 	
 	/* sets asside some space to make these objects will be declared later  */
 	
-	DifferentialDrive lifterBase;
+	public DifferentialDrive lifterBase;
 
 	
 	/* select correct controller for robot configuration  */
-	private VictorSP topLifterMotor;
-	private VictorSP bottomLifterMotor;
-	// private Spark topLifterMotor;
-	// private Spark bottomLifterMotor;
+	//private VictorSP topLifterMotor;
+	//private VictorSP bottomLifterMotor;
+	private Spark topLifterMotor;
+	private Spark bottomLifterMotor;
 
 	
 	
 
 	/* this is the constructor that 'builds' the object when called  */
 	public BoxLifter(){
-
-		topLifterMotor = new VictorSP(RobotMap.RIGHT_TOP_LIFTER_MOTOR_ID);
-		bottomLifterMotor = new VictorSP(RobotMap.RIGHT_BOTTOM_LIFTER_MOTOR_ID);
-		//topLifterMotor = new Spark(RIGHT_TOP_LIFTER_MOTOR_ID);
-		//bottomLifterMotor = new Spark(RIGHT_BOTTOM_LIFTER_MOTOR_ID);
-		
+		super("BoxLifter", 2.0, 0.0, 0.0 );
+		setAbsoluteTolerance(0.05);
+		getPIDController().setContinuous(false);
+		//topLifterMotor = new VictorSP(RobotMap.PWM_PinOut.RIGHT_TOP_LIFTER_MOTOR_ID);
+		//bottomLifterMotor = new VictorSP(RobotMap.PWM_PinOut.RIGHT_BOTTOM_LIFTER_MOTOR_ID);
+		topLifterMotor = new Spark(RobotMap.PWM_PinOut.RIGHT_TOP_LIFTER_MOTOR_ID);
+		bottomLifterMotor = new Spark(RobotMap.PWM_PinOut.RIGHT_BOTTOM_LIFTER_MOTOR_ID);		
 		bottomLifterMotor.setInverted(true);
-
 		lifterBase = new DifferentialDrive(topLifterMotor, bottomLifterMotor);
-
 		lifterBase.setExpiration(0.1);
 	
 }
@@ -55,6 +50,24 @@ public class BoxLifter extends PIDSubsystem{
 
 		lifterBase.arcadeDrive(lowerPower, 0);	
 	
+	}
+
+	@Override
+	protected double returnPIDInput() {
+		// TODO Auto-generated method stub
+		return RobotIO.getRobotLifterGyroAngle();
+	}
+
+	@Override
+	protected void usePIDOutput(double output) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	protected void initDefaultCommand() {
+		// TODO Auto-generated method stub
+		
 	}
 		
 }
