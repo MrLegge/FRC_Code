f0f3b72342368c99a3e3b2c373a2812311d9b622
Mr Phil Legge <phil.legge@det.nsw.edu.au>
2018-03-01 15:25:57 +1100

    Update RobotMap.java

--- FRC2018/src/org/usfirst/frc/team6007/robot/RobotMap.java
+++ FRC2018/src/org/usfirst/frc/team6007/robot/RobotMap.java
@@ -11,25 +11,29 @@ package org.usfirst.frc.team6007.robot;
 	/*********************************
 	*  Change these depending on pin configuration on RoboRIO *** 
 	*********************************/
-	/**** PWM PinOut ******/
-	final int REAR_LEFT_MOTOR_ID = 0;
-	final int FRONT_LEFT_MOTOR_ID = 1;
-	final int FRONT_RIGHT_MOTOR_ID = 2;
-	final int REAR_RIGHT_MOTOR_ID = 3;	
-	final int RIGHT_TOP_LIFTER_MOTOR_ID = 4;
-	final int RIGHT_BOTTOM_LIFTER_MOTOR_ID = 5;
-	final int LEFT_GRABBER_MOTOR_ID = 6;
-	final int RIGHT_GRABBER_MOTOR_ID = 7;
-	
-	/**** DIO PinOut ******/
-	final int RIGHT_MOTOR_ENCODER_A_CHANNEL = 0;
-	final int RIGHT_MOTOR_ENCODER_B_CHANNEL = 1;
-	final int LEFT_MOTOR_ENCODER_A_CHANNEL = 2;
-	final int LEFT_MOTOR_ENCODER_B_CHANNEL = 3;
-	final int LIFTER_MOTOR_ENCODER_A_CHANNEL = 4;
-	final int LIFTER_MOTOR_ENCODER_B_CHANNEL = 5;
-	
-	/*****Analog PinOut******/
-	final int ROBOT_LIFTER_GYRO = 0;
+	public class PWM_PinOut {
+		final static int REAR_LEFT_MOTOR_ID = 0;
+		final static int FRONT_LEFT_MOTOR_ID = 1;
+		final static int FRONT_RIGHT_MOTOR_ID = 2;
+		final static int REAR_RIGHT_MOTOR_ID = 3;	
 		
+		final static int RIGHT_TOP_LIFTER_MOTOR_ID = 4;
+		final static int RIGHT_BOTTOM_LIFTER_MOTOR_ID = 5;
+		
+		final static int LEFT_GRABBER_MOTOR_ID = 6;
+		final static int RIGHT_GRABBER_MOTOR_ID = 7;
+	}
+	public class DIO_PinOut{
+		final static int RIGHT_MOTOR_ENCODER_A_CHANNEL = 0;
+		final static int RIGHT_MOTOR_ENCODER_B_CHANNEL = 1;
+		
+		final static int LEFT_MOTOR_ENCODER_A_CHANNEL = 2;
+		final static int LEFT_MOTOR_ENCODER_B_CHANNEL = 3;
+		
+		final static int LIFTER_MOTOR_ENCODER_A_CHANNEL = 4;
+		final static int LIFTER_MOTOR_ENCODER_B_CHANNEL = 5;
+	}	
+	public class Analog_PinOut{
+		final static int ROBOT_LIFTER_GYRO = 0;
+	}
 }	
