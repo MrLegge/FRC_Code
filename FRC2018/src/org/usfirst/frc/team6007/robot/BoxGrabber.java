0b5dc333c0b560e425ee5bef89a8d572ec0e8b8c
Mr Phil Legge <phil.legge@det.nsw.edu.au>
2018-03-07 15:37:50 +1100

    Update BoxGrabber.java

--- FRC2018/src/org/usfirst/frc/team6007/robot/BoxGrabber.java
+++ FRC2018/src/org/usfirst/frc/team6007/robot/BoxGrabber.java
@@ -4,7 +4,7 @@ package org.usfirst.frc.team6007.robot;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive; 
 //import edu.wpi.first.wpilibj.VictorSP;
-//import edu.wpi.first.wpilibj.Spark;
+import edu.wpi.first.wpilibj.Spark;
 import edu.wpi.first.wpilibj.command.Subsystem;
 
 // this is the declaraion of the class everything must be within these curly braces
@@ -15,8 +15,8 @@ public class BoxGrabber extends Subsystem{
 	/* motor controllers*/
 	//private VictorSP leftGrabberMotor;
 	//private VictorSP rightGrabberMotor;
-	//private Spark leftGrabberMotor;
-//	private Spark rightGrabberMotor;
+	private Spark leftGrabberMotor;
+	private Spark rightGrabberMotor;
 
 	
 	
@@ -26,10 +26,10 @@ public class BoxGrabber extends Subsystem{
 	/* this is where the motorcontrollers above get created but using the 'new' keyword and including the needed parametres*/
 	//leftGrabberMotor = new VictorSP(RobotMap.PWM_PinOut.LEFT_GRABBER_MOTOR_ID);
 	//rightGrabberMotor = new VictorSP(RobotMap.PWM_PinOut.RIGHT_GRABBER_MOTOR_ID);
-	//leftGrabberMotor = new Spark(RobotMap.LEFT_GRABBER_MOTOR_ID);
-	//rightGrabberMotor = new Spark(RobotMap.RIGHT_GRABBER_MOTOR_ID);		
+	leftGrabberMotor = new Spark(RobotMap.PWM_PinOut.LEFT_GRABBER_MOTOR_ID);
+	rightGrabberMotor = new Spark(RobotMap.PWM_PinOut.RIGHT_GRABBER_MOTOR_ID);		
 	
-	grabberBase = RobotMap.grabberBase;	
+	grabberBase = new DifferentialDrive(leftGrabberMotor, rightGrabberMotor);	
 	}
 	
 	/* function to pull the power cube into the holder*/
@@ -44,6 +44,7 @@ public class BoxGrabber extends Subsystem{
 	
 	/*this will shuffle the cube around by sucking in and out to align it properly*/
 	public void shuffle(){
+		
 			grabberBase.arcadeDrive(1, 0);
 			Timer.delay(0.2);
 			grabberBase.arcadeDrive(-1, 0);
@@ -53,6 +54,7 @@ public class BoxGrabber extends Subsystem{
 			grabberBase.arcadeDrive(-1, 0);
 			Timer.delay(0.5);
 			grabberBase.arcadeDrive(0, 0);
+			
 	}
 
 	@Override
