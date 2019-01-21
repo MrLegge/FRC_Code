fa5e4ed2ee51511f9c291f145cef9a5aa3418bf7
Mr Phil Legge <phil.legge@det.nsw.edu.au>
2019-01-21 11:42:44 +1100

    Update HatchDelivery.java

--- FRC2019/src/org/usfirst/frc/team6007/robot/HatchDelivery.java
+++ FRC2019/src/org/usfirst/frc/team6007/robot/HatchDelivery.java
@@ -8,7 +8,7 @@ package org.usfirst.frc.team6007.robot;
 
 public class HatchDelivery{
   
-	private static RobotIO robotLifterPotentiometer;
+	private static RobotIO hatchtLifterPotentiometer;
   private static ReadSwitch homePosition;
   private static ReadSwitch floorPosition;
 
