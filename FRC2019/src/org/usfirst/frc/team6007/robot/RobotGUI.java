65fe8e01d463727c0c8edc0d6466a4fa93167665
DanielElliott4 <36021880+DanielElliott4@users.noreply.github.com>
2019-01-21 11:41:49 +1100

    Update RobotGUI.java
    This lit fam

--- FRC2019/src/org/usfirst/frc/team6007/robot/RobotGUI.java
+++ FRC2019/src/org/usfirst/frc/team6007/robot/RobotGUI.java
@@ -1,4 +1,38 @@
-/***********************************************************
-* this is just an idea for the 2019 comp 
-* Date: 1-11-2018
-************************************************************/
+package org.usfirst.frc.team6007.robot;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+public class RobotGUI{
+//variable spot?
+
+
+	public RobotGUI(){
+	//the something goes here?
+	
+	}
+		
+	public void getData(){
+		
+	startPos = SmartDashboard.getString("DB/String 0", "There is nothing there");
+		
+		//gets data from the dashboard, 
+		//if there is nothing there it gives the defult data
+			
+	}
+	
+	public void switchController(){
+		
+		isXboxController = SmartDashboard.putBoolean("DB/Button 0", false);
+		//if return is true, the Xbox controller is used, if false joystick is used
+
+	}
+	
+	public void sendData(){
+		
+		SmartDashboardputNumber("DB/String 5", "420");
+		//sends data to the dashboard
+			
+	}
+		
+	
+	
+}	
