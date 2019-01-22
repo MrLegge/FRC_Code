1ce1752dec86f6eb91e52a004ca7985f6c3709b5
Mr Phil Legge <phil.legge@det.nsw.edu.au>
2019-01-22 11:22:47 +1100

    Update BallDelivery.java

--- FRC2019/src/org/usfirst/frc/team6007/robot/BallDelivery.java
+++ FRC2019/src/org/usfirst/frc/team6007/robot/BallDelivery.java
@@ -9,8 +9,8 @@ package org.usfirst.frc.team6007.robot;
 public class BallDelivery{
   
 	private static RobotIO ballLifterPotentiometer;
-  private static ReadSwitch homePosition;
-  private static ReadSwitch floorPosition;
+  	private static ReadSwitch homePosition;
+  	private static ReadSwitch floorPosition;
 
 	public BallDelivery(){
   
