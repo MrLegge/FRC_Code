ca640b159324e0fcef11ec67e1811082d1454860
Mr Phil Legge <phil.legge@det.nsw.edu.au>
2019-01-22 11:36:57 +1100

    Update and rename BallDelivery.java to CargoDelivery.java

---
+++ FRC2019/src/org/usfirst/frc/team6007/robot/CargoDelivery.java
@@ -0,0 +1,34 @@
+/***********************************************************
+* 
+* 
+************************************************************/
+
+package org.usfirst.frc.team6007.robot;
+
+
+public class CargoDelivery{
+  
+	private static RobotIO ballLifterPotentiometer;
+  	private static RobotIO robotIO;
+
+	public CargolDelivery(){
+  		robotIO = new RobotIO();
+    	}
+	
+	public void cargoIntake(){
+		while(robotIO.?reedSwitch?.get()){
+		
+		}
+	}
+	
+  	public void cargoLoad(){
+		while(robotIO.?poteniometervalue?.get >= ?value? && robotIO.?poteniometervalue?.get <= ?value?){
+		}
+	}
+	
+	public void loaderToHome(){
+		while(robotIO.?reedSwitch?.get()){
+		
+		}
+	}
+  }
