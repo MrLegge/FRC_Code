fa7520a31bff79e3a2c047ff77cf37c6a28aa682
Ebony Nicholas <35989154+EbonyN@users.noreply.github.com>
2019-02-16 08:53:25 +1100

    Update PneumaticDelivery.java

--- main/java/frc/robot/PneumaticDelivery.java
+++ main/java/frc/robot/PneumaticDelivery.java
@@ -4,51 +4,30 @@ Pneumatic file for delivering hatch from robot
 
 package frc.robot;
 
-//i couldnt find the library because im a chungus
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.Solenoid;
-import edu.wpi.first.wpilibj.Compressor;
 
 public class PneumaticDelivery{
   //i dont know if anything extra goes here
-  private Solenoid leftPneumatic;
-  private Solenoid middlePneumatic;
-  private Solenoid rightPneumatic;
-  private Compressor compressor;
+  private Solenoid pushingPneumatic;
+  private Solenoid pullingPneumatic;
   
   public PneumaticDelivery(){
-	//does that thing and get the ports from robotMAP
-    leftPneumatic = new Solenoid(1);
-    middlePneumatic = new Solenoid(2);
-    rightPneumatic = new Solenoid(3);
-    compressor = new Compressor(4);
+
+	pushingPneumatic = new Solenoid(0); //pushes the solenoids out and is the left controller
+	pullingPneumatic = new Solenoid(1); //pulls the solenoids in and is the right controller
+
 }
 
-	public void pushOut(){//just pushes it out
-		leftPneumatic.set(true);
-		middlePneumatic.set(true);
-		rightPneumatic.set(true);   
-	} 
-
-	public void pullIn(){// just pulls it in
-		leftPneumatic.set(false);
-		middlePneumatic.set(false);
-		rightPneumatic.set(false);
-	}
-
 	public void pushAndPull(double delay){//pushes it out and pulls it in 
-		leftPneumatic.set(true);			  //after a chosen amount of time
-		middlePneumatic.set(true);
-		rightPneumatic.set(true); 
-		Timer.delay(delay);
-		leftPneumatic.set(false);
-		middlePneumatic.set(false);
-		rightPneumatic.set(false);		
+
+		pushingPneumatic.set(true);
+		Timer.delay(delay);			//fires out
+		pushingPneumatic.set(false);
+		
+		pullingPneumatic.set(true);	
+		Timer.delay(delay);			//pulls in
+		pullingPneumatic.set(false);	
 	}
 
-	public void turnCompressorOn(double timeOn){
-		compressor.setClosedLoopControl(true);
-		Timer.delay(timeOn);
-		compressor.setClosedLoopControl(false);
-	}
 }
