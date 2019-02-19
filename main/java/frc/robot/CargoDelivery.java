e70c5f7f7176c9d6f88926970f92604a465b009b
DanielElliott4 <36021880+DanielElliott4@users.noreply.github.com>
2019-02-19 17:47:17 +1100

    Update CargoDelivery.java

--- main/java/frc/robot/CargoDelivery.java
+++ main/java/frc/robot/CargoDelivery.java
@@ -1,28 +1,27 @@
 /*******************************************************
-* Robotics Hatch Delivery by everyone except Jordan Thorne date 23/01/19	
+* Robotics Hatch Delivery by Jordan Thorne date 23/01/19	
 *******************************************************/
 /***********************************
 *Arm that retrives and delivers disk
 ***********************************/
 package frc.robot;
+import edu.wpi.first.wpilibj.PIDController;
+import edu.wpi.first.wpilibj.SpeedControllerGroup;
 import edu.wpi.first.wpilibj.VictorSP;
-import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 
 public class CargoDelivery extends Kevin{
-	private static VictorSP cargoDeliveryMotor;
+	public static VictorSP cargoDeliveryMotor;
 
-	public CargoDelivery(RobotIO rio, VictorSP tkm, VictorSP bkm, DifferentialDrive kb, int off){
+	public CargoDelivery(RobotIO rio, VictorSP tkm, VictorSP bkm, SpeedControllerGroup kms, int off, PIDController kc){
 		//super(new RobotIO(), new VictorSP(RobotMap.PWM_PinOut.TOP_HATCH_MOTOR_ID), new VictorSP(RobotMap.PWM_PinOut.BOTTOM_HATCH_MOTOR_ID), new DifferentialDrive(topKevinMotor, bottomKevinMotor), 10);
-		super(rio, tkm, bkm, kb, off);
-		cargoDeliveryMotor = new VictorSP(5);
-
+		super(rio, tkm, bkm, kms, off, kc);
+		cargoDeliveryMotor = new VictorSP(RobotMap.PWM_PinOut.CARGO_SHOOTER_MOTOR_ID);
 
 	}
-/*
+
 	public static void liftToCargoPosition(int target){
-		Robot.kevin.liftToPosition(target);
+		liftToPosition(target);
 		}
-*/
 
 	public static void shootCargo(){
 		cargoDeliveryMotor.set(1.0); //shhots out ball
